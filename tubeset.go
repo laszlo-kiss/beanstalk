2fb8fdcd7ed2439b842e322473a320cac4ea22a4
Laszlo Kiss <lkiss@deltaprime.com>
2016-02-21 12:05:11 -0700

    Added method to get a reservation without having to specify a timeout.

--- tubeset.go
+++ tubeset.go
@@ -37,3 +37,20 @@ func (t *TubeSet) Reserve(timeout time.Duration) (id uint64, body []byte, err er
 	}
 	return id, body, nil
 }
+
+
+// Reserve reserves and returns a job from one of the tubes in t.
+//
+// Typically, a client will reserve a job, perform some work, then delete
+// the job with Conn.Delete.
+func (t *TubeSet) ReserveNoTimeout() (id uint64, body []byte, err error) {
+	r, err := t.Conn.cmd(nil, t, nil, "reserve")
+	if err != nil {
+		return 0, nil, err
+	}
+	body, err = t.Conn.readResp(r, true, "RESERVED %d", &id)
+	if err != nil {
+		return 0, nil, err
+	}
+	return id, body, nil
+}
